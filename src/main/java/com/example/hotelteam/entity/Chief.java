88ff9ab3595e97bb5eacbc615b2f6daad6006e34
book <pik0329@naver.com>
2025-03-20 14:47:09 +0900

    ddddd

--- src/main/java/com/example/hotelteam/entity/Chief.java
+++ src/main/java/com/example/hotelteam/entity/Chief.java
@@ -2,6 +2,7 @@ package com.example.hotelteam.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -14,7 +15,7 @@ import lombok.*;
 public class Chief {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chiefNum;
     private String chiefName;
 }
