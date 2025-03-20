dc82150c6def9fa09bb82b32407d142afb341812
sin@sin <sin@sin.sin>
2025-03-20 17:56:17 +0900

    dto호텔참조 추가

--- src/main/java/com/example/hotelteam/dto/BoardDTO.java
+++ src/main/java/com/example/hotelteam/dto/BoardDTO.java
@@ -1,5 +1,9 @@
 package com.example.hotelteam.dto;
 
+import com.example.hotelteam.entity.Hotel;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,4 +17,7 @@ public class BoardDTO {
     private Long boardNum;
     private String boardTitle;
     private String boardContent;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_num")
+    private Hotel hotel;
 }
