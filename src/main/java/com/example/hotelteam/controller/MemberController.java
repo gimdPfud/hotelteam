e7bee916ae17c17e7f003fb99ffe462e4dade6e2
book <pik0329@naver.com>
2025-03-20 14:51:22 +0900

    ddddd

--- src/main/java/com/example/hotelteam/controller/MemberController.java
+++ src/main/java/com/example/hotelteam/controller/MemberController.java
@@ -50,4 +50,6 @@ public class MemberController {
         model.addAttribute("memberList", memberList);
         return "member/permission";
     }
+
+
 }
--- src/main/java/com/example/hotelteam/service/ChiefService.java
+++ src/main/java/com/example/hotelteam/service/ChiefService.java
@@ -1,4 +1,9 @@
 package com.example.hotelteam.service;
 
+import com.example.hotelteam.dto.ChiefDTO;
+
 public interface ChiefService {
+
+    public void chief(ChiefDTO chiefDTO);
+
 }
