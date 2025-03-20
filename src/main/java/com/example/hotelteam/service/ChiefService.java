a3185e8030769dbbb43afc51307d42f063904fc9
book <pik0329@naver.com>
2025-03-20 18:00:31 +0900

    이걸로 푸쉬

--- src/main/java/com/example/hotelteam/service/ChiefService.java
+++ src/main/java/com/example/hotelteam/service/ChiefService.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.security.Principal;
+import java.util.List;
 
 public interface ChiefService {
 
-    public Page<ChiefDTO> getCheifList(Principal principal, Pageable pageable);
+    public List<ChiefDTO> getChiefList(Principal principal, Pageable pageable);
 
     public void addChief(ChiefDTO chiefDTO, Principal principal);
 
