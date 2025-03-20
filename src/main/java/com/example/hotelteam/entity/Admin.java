e021d697d4a5bd5ac338520947611c4a263b126c
segong <sy981022@naver.com>
2025-03-20 11:46:15 +0900

    Admin entity, DTO 작성

---
+++ src/main/java/com/example/hotelteam/entity/Admin.java
@@ -0,0 +1,32 @@
+package com.example.hotelteam.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_num")
+    Long adminNum;
+
+    @Column(length = 20)
+    String adminName;
+
+    @Column(length = 100)
+    String adminAddr;
+
+    @ManyToOne
+    @JoinColumn(name = "chiefNum")
+    private Chief chief;
+
+}
