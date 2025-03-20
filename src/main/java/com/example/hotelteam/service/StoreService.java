4ce487eda6e916cb1f3cff5cc089f9e6fd2024bc
예령 <gimdPfud@gmail.com>
2025-03-20 11:32:58 +0900

    가맹직영점 service 설계

---
+++ src/main/java/com/example/hotelteam/service/StoreService.java
@@ -0,0 +1,14 @@
+package com.example.hotelteam.service;
+
+public interface StoreService {
+    //가맹직영점 등록
+    public Long register(StoreDTO storeDTO);
+
+    //가맹직영점 수정(담당자 정보 수정?)
+
+    //가맹직영점 삭제
+
+    //가맹직영점 상세보기
+
+    //가맹직영점 목록 보기
+}
