e85ff0aae5ed50d2e9a09f47eaae5a3fb1481956
book <pik0329@naver.com>
2025-03-21 09:41:27 +0900

    대충 로그인한사람 명단만 보이게 변경 + 한 회장단 총판 3개 이상은 등록 안되게 방지

--- src/main/java/com/example/hotelteam/service/ChiefServiceImpl.java
+++ src/main/java/com/example/hotelteam/service/ChiefServiceImpl.java
@@ -2,14 +2,12 @@ package com.example.hotelteam.service;
 
 import com.example.hotelteam.dto.ChiefDTO;
 import com.example.hotelteam.entity.Chief;
-import com.example.hotelteam.entity.Member;
 import com.example.hotelteam.repository.ChiefRepository;
 import com.example.hotelteam.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -27,26 +25,23 @@ public class ChiefServiceImpl implements ChiefService {
     private final ModelMapper modelMapper = new ModelMapper();
     private final ChiefRepository chiefRepository;
 
-
     public List<ChiefDTO> getChiefList(Principal principal, Pageable pageable) {
-//        Page<Chief> pagelist = chiefRepository.findAll(pageable);
-//        Page<ChiefDTO> pageDTOList = pagelist.map(chief -> modelMapper.map(chief, ChiefDTO.class));
-
-                String loggedInUserEmail = principal.getName();
-        Member loggedInUser = memberRepository.findByMemberEmail(loggedInUserEmail).get();
-        List<Chief> chiefList = chiefRepository.findByMember_MemberNum(loggedInUser.getMemberNum());
+        log.info("맴버메일 : {}" , principal.getName());
+        List<Chief> chiefList = chiefRepository.findByMember_MemberEmail(principal.getName());
         List<ChiefDTO> chiefDTOList = new ArrayList<>();
          for (Chief chief : chiefList) {
              chiefDTOList.add(modelMapper.map(chief, ChiefDTO.class));
          }
-//
         return chiefDTOList;
     }
 
     public void addChief(ChiefDTO chiefDTO, Principal principal) {
-        Chief chieft = modelMapper.map(chiefDTO, Chief.class);
-        chiefRepository.save(chieft);
 
+        if (chiefRepository.findByMember_MemberEmail(principal.getName()).size() >= 3) {
+        } else {
+            Chief chieft = modelMapper.map(chiefDTO, Chief.class);
+            chiefRepository.save(chieft);
+        }
     }
 
     public void updateChief(ChiefDTO chiefDTO) {
